5204b9fd6487a69b21d14a7827754b03dc4ca6f6
benjamin.boutrois28@gmail.com <benjamin.boutrois28@gmail.com>
2023-09-29 22:44:49 +0200

    Fixed not refreshing if applied once in combat

--- WarCryIcon.lua
+++ WarCryIcon.lua
@@ -1,14 +1,13 @@
-DEFAULT_CHAT_FRAME:AddMessage("test")
-
 local wci = {}
 
+wci.frame = CreateFrame("Frame", "FramePlaceholder", UIParent)
 wci.iconFrame = CreateFrame("Frame", "WarCryIconFrame", UIParent)
-wci.iconFrame:SetWidth(32)
-wci.iconFrame:SetHeight(32)
-wci.iconFrame:SetScript("OnEvent", function() wci[event](wci, arg2, arg3) end)
-wci.iconFrame:SetScript("OnUpdate", function() wci["OnUpdate"]() end)
-wci.iconFrame:RegisterEvent("PLAYER_REGEN_DISABLED")
-wci.iconFrame:RegisterEvent("PLAYER_REGEN_ENABLED")
+wci.iconFrame:SetWidth(40)
+wci.iconFrame:SetHeight(40)
+wci.frame:SetScript("OnEvent", function() wci[event](wci, arg2, arg3) end)
+wci.frame:SetScript("OnUpdate", function() wci["OnUpdate"]() end)
+wci.frame:RegisterEvent("PLAYER_REGEN_DISABLED")
+wci.frame:RegisterEvent("PLAYER_REGEN_ENABLED")
 wci.iconFrame:SetPoint("CENTER", 0, 150)
 
 local iconTexture = wci.iconFrame:CreateTexture(nil, "BACKGROUND")
@@ -26,7 +25,7 @@ local function UpdateIconVisibility()
 
             if name == "Interface\\Icons\\Ability_Warrior_BattleShout" then
                 hasBattleShout = true
-                DEFAULT_CHAT_FRAME:AddMessage("Battle Shout found")
+                -- DEFAULT_CHAT_FRAME:AddMessage("Battle Shout found")
                 break
             end
         end
@@ -46,8 +45,6 @@ local lastMessageTime = 0
 
 function wci:OnUpdate()
     if not lastMessageTime or lastMessageTime < GetTime() - 1 then
-        DEFAULT_CHAT_FRAME:AddMessage("lastMessageTime : " .. tostring(lastMessageTime))
-        DEFAULT_CHAT_FRAME:AddMessage("GetTime() : " .. tostring(GetTime()))
         lastMessageTime = GetTime()
         --DEFAULT_CHAT_FRAME:AddMessage("Update WCI")
         UpdateIconVisibility()
@@ -55,7 +52,7 @@ function wci:OnUpdate()
 end
 
 function wci:PLAYER_REGEN_DISABLED()
-    DEFAULT_CHAT_FRAME:AddMessage("PLAYER_REGEN_DISABLED triggered")
+    -- DEFAULT_CHAT_FRAME:AddMessage("PLAYER_REGEN_DISABLED triggered")
 
     UpdateIconVisibility()
 end
